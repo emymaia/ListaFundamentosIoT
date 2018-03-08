bd8779f1c9b0fcdd968b0ab5479dba9c2809349f
Emy.Maia <31630349+emymaia@users.noreply.github.com>
2018-03-08 13:55:56 -0300

    Update DecimalParaBinario.c
    Versão concluída

--- DecimalParaBinario.c
+++ DecimalParaBinario.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(void){
-    int N1, Resultado, Contador,C1;
+int main(){
+    int N1, Resultado, Contador;
     int Resto[N1];
     printf("Digite o número: \n");
     scanf("%d", &N1);
@@ -15,16 +15,17 @@ int main(void){
         Resto[Contador] = N1%2;
         N1 = Resultado;
         Contador += 1;
-        if(Resultado<=0) {
-            printf("%d\n", Resultado);
-            for (C1 = Resto[Contador]; C1 == Resto[0]; C1 -= 1) {
-                printf("%d\n", Resto[Contador]);
-            }
+        if(Resultado == 0) {
+            Contador = Contador - 1;
+            while (0 <= Contador) {
+                printf("%d", Resto[Contador]);
+                Contador = Contador - 1;
         }
-        else{}
-        }
+      }
+    }
 
 
 
     getchar();
     return 0;
+}
